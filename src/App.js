7ce9edd519e3763776da3afb758f2d4979489faf
sa48k <sa48k@users.noreply.github.com>
2021-08-13 20:40:01 +1200

    Figuring out onClicks

--- src/App.js
+++ src/App.js
@@ -6,23 +6,38 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core'
+import { ThemeProvider, createTheme } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import orange from '@material-ui/core/colors/orange';
 import './index.css'
+import YearGoals from './Accordion.js'
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: orange,
   },
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
 class YearButton extends React.Component {
+  handleClick() {
+    alert('now we can hide the YearMenu and bring up the ACCORDION OF GOALS for year ' + this.props.year);
+  }
+
   render() {
     return (
       <Grid item xs={4} >
-        <Button p={5} fullWidth="true" className="huge-button hvr-push" variant="contained" color="primary">
-          <Typography variant="h4" align="center" >
+        <Button fullWidth={true} className="huge-button" onClick={() => this.handleClick()} variant="contained" color="primary">
+          <Typography variant="h5" align="center" >
             Year {this.props.year}
           </Typography>
         </Button>
@@ -31,22 +46,29 @@ class YearButton extends React.Component {
   }
 }
 
+class YearMenu extends React.Component {
+  render() {
+    return (
+      <Container maxWidth="sm">
+        <ThemeProvider theme={theme}>
+          <Grid container spacing={1}>
+            <YearButton year="3" />
+            <YearButton year="5" />
+            <YearButton year="7" />
+          </Grid>
+          <Grid container spacing={1}>
+            <YearButton year="4"/>
+            <YearButton year="6"/>
+            <YearButton year="8"/>
+          </Grid>
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <ThemeProvider theme={theme}>
-        <Grid container spacing={1}>
-          <YearButton year="3" />
-          <YearButton year="5" />
-          <YearButton year="7" />
-        </Grid>
-        {/*<br />*/}
-        <Grid container spacing={1}>
-          <YearButton year="4"/>
-          <YearButton year="6"/>
-          <YearButton year="8"/>
-        </Grid>
-      </ThemeProvider>
-    </Container>
+    <YearMenu />
   );
 }
