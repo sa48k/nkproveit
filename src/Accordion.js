b672b310c9acc6860ec94d1fa9ccb559598574c8
sa48k <sa48k@users.noreply.github.com>
2021-08-17 22:59:49 +1200

    The accordion: it's ugly, but it's there

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';import { makeStyles } from '@material-ui/core/styles';
 import axios from "axios";
+import Button from '@material-ui/core/Button';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
@@ -11,22 +12,33 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
   heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(16),
+    fontWeight: theme.typography.fontWeightBold,
   },
 }));
 
 export default function YearGoals(props) {
   const classes = useStyles();
-  const [yearLevel, setYearLevel] = useState(0);
+  // const [yearLevel, setYearLevel] = useState(0);
 
-  const url = 'http://127.0.0.1:5000/y3/numseq4';
+  const url = 'http://127.0.0.1:5000/y' + props.year + '/numseq4';
   useEffect(() => {
     axios.get(url).then((response) => {
       console.log(response.data);
     });
   }, []);
 
+  function GoalButton(props) {
+    return (
+      <AccordionDetails>
+        <Button fullWidth={true} className="big-button" variant="contained" color="primary">
+          <Typography align="left" variant="body2" >
+            {props.text}
+          </Typography>
+        </Button>
+      </AccordionDetails>
+    );
+  }
 
   return (
     <div className={classes.root}>
@@ -35,30 +47,43 @@ export default function YearGoals(props) {
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>Accordion 1</Typography>
+          id="panel1a-header">
+          <Typography className={classes.heading}>Number Identification</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            WOMP WOMP
-          </Typography>
-        </AccordionDetails>
+        <GoalButton text="Identify and write numbers to 1,000" />
       </Accordion>
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>Accordion 2</Typography>
+          id="panel2a-header">
+          <Typography className={classes.heading}>Number Sequence and Order</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        <GoalButton text="Count forwards and say the number one, ten and a hundred after" />
+        <GoalButton text="Count forwards and say the number one, ten and a hundred before" />
+        <GoalButton text="Order numbers" />
+        <GoalButton text="Round three-digit numbers to the nearest ten and hundred" />
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header">
+          <Typography className={classes.heading}>Place Value</Typography>
+        </AccordionSummary>
+        <GoalButton text="Explain the place value of hundreds" />
+        <GoalButton text="Recall the number of tens and hundreds in three-digit numbers" />
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4a-content"
+          id="panel4a-header">
+          <Typography className={classes.heading}>Groupings and Basic Facts</Typography>
+        </AccordionSummary>
+        <GoalButton text="Recall addition facts to 20" />
+        <GoalButton text="Recall subtraction facts to 20" />
+        <GoalButton text="Recall groupings within 100 - multiples of 5" />
       </Accordion>
     </div>
   );
